a0e3d273c7a39c9f29250fd2bd624bc626916249
Alex Simko <alexsimko4@gmail.com>
2026-01-29 17:05:12 -0500

    feat(dev-mode): added electron dev mode.
    In dev mode electron will spin up a local vite instance with live reload, instead of relying on build files during development

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,7 @@
 import { app, BrowserWindow } from 'electron';
+import isDev from 'electron-is-dev';
+
+console.log("Is Dev Mode: ", isDev);
 
 const PORT = process.env.VITE_DEV_SERVER_PORT || '5173';
 
@@ -11,7 +14,11 @@ function createWindow () {
     }
   })
 
-  win.loadURL(`http://localhost:${PORT}`);
+  if (isDev) {
+    win.loadURL(`http://localhost:${PORT}`);
+  } else {
+    win.loadFile('./dist/index.html');
+  }
 }
 
 app.whenReady().then(createWindow)
