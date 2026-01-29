2b2aa7a82d942e48b125139a491d09d5cfb71bcd
Alex Simko <alexsimko4@gmail.com>
2026-01-29 17:05:40 -0500

    chore(debug): removed debug line

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,8 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 import isDev from 'electron-is-dev';
 
-console.log("Is Dev Mode: ", isDev);
-
 const PORT = process.env.VITE_DEV_SERVER_PORT || '5173';
 
 function createWindow () {
