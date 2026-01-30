a84d047154025edc097775d1fe2e73b0200597bc
Alex Simko <alexsimko4@gmail.com>
2026-01-30 03:00:10 -0500

    ci(build-script): added a build all argument & allowed programatic building of react via function.

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,6 +5,10 @@ switch (command) {
     case 'python':
         buildPython();
         break;
+    case 'all':
+        buildPython();
+        buildReact();
+        break;
 }
 
 function buildPython() {
@@ -26,6 +30,22 @@ function buildPython() {
             console.log("Python backend built successfully.");
         } else {
             console.error(`Python backend build failed with code ${code}.`);
+        }
+    });
+}
+
+function buildReact() {
+    console.log("Building React frontend...");
+    const viteBuildProcess = spawn('yarn', ['run', 'build:react'], {
+        stdio: 'inherit',
+        shell: true,
+    });
+
+    viteBuildProcess.on('close', (code) => {
+        if (code === 0) {
+            console.log("React frontend built successfully.");
+        } else {
+            console.error(`React frontend build failed with code ${code}.`);
         }
     });
 }
