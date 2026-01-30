710ed675dfe1c5e84ff9f38212306882a77a29a5
Alex Simko <alexsimko4@gmail.com>
2026-01-30 01:27:47 -0500

    cI(python-build): added python build script

---
+++ scripts/build.ts
@@ -0,0 +1,24 @@
+import { spawn } from 'child_process';
+const [ , , command ] = process.argv;
+
+switch (command) {
+    case 'python':
+        buildPython();
+        break;
+}
+
+function buildPython() {
+    // Step 3: Run pyinstaller using venv's python
+    const pyInstallerProcess = spawn('.venv\\Scripts\\python', ['-m', 'PyInstaller', '--onefile', 'src/backend/main.py'], {
+        stdio: 'inherit',
+        shell: true,
+    });
+
+    pyInstallerProcess.on('close', (code) => {
+        if (code === 0) {
+            console.log("Python backend built successfully.");
+        } else {
+            console.error(`Python backend build failed with code ${code}.`);
+        }
+    });
+}
