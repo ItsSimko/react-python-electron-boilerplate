e0f9621e29b8507bc9815cbfb82127c517cecf14
Alex Simko <alexsimko4@gmail.com>
2026-01-30 01:41:45 -0500

    feat(renderer): updated base react to use python flask api for count variable.
    This showcases communication between python and react usign electron

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  // get count from python api
   const [count, setCount] = useState(0)
+  
+  // update count on app launch based on server sided var
+  useEffect(() => {
+    fetchCount()
+  }, [])
+
+  // Fetch count from Python API
+  const fetchCount = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/count')
+      setCount(response.data.count)
+    } catch (error) {
+      console.error('Error fetching count:', error)
+    }
+  }
+
+  const updateCount = (increment: number) => {
+    axios.post('http://localhost:5000/count', { increment: increment }).then(() => {
+      setCount(count + increment)
+    }).catch((error) => {
+      console.error('Error updating count:', error)
+    })
+  }
 
   return (
     <>
@@ -18,7 +43,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => updateCount(1)}>
           count is {count}
         </button>
         <p>
